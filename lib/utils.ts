ef12ccaea1a58f8d23bf68a74396e7abf338132f
Brown248 <suwat.you248@gmail.com>
2025-12-18 20:54:57 +0700

    code

--- lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-// ฟังก์ชันช่วยรวม Class Tailwind ให้ไม่ตีกัน (มาตรฐาน Next.js)
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
